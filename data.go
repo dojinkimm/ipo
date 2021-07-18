d6b3991868eed6d0d07780b4ad0a581386756d97
dojinkimm <dojinkim119@gmail.com>
2021-07-18 20:25:31 +0900

    :recycle:   Add ipos.json file and refactor code to handle json file

--- data.go
+++ data.go
@@ -1,44 +1,47 @@
 package ipo
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
 type IpoMessage struct {
 	// 종목명
 	ItemName string `json:"item_name"`
 	// 종목코드
 	TickerCode string `json:"ticker_code"`
 	// 공모 청약 시작일
-	StartDateMs int64 `json:"start_date_ms"`
+	StartDate string `json:"start_date"`
 	// 공모 청약 종료일
-	EndDateMs int64 `json:"end_date_ms"`
-	// 환불일
-	RefundDateMs int64 `json:"refund_date_ms"`
-	// 상장일
-	ListingDateMs int64 `json:"listing_date_ms"`
+	EndDate string `json:"end_date"`
 	// 확정공모가
-	FinalOfferingPrice int64 `json:"final_offering_price"`
+	FinalOfferingPrice *int64 `json:"final_offering_price"`
 	// 희망공모가 하한
 	OfferingRangeLowerLimit int64 `json:"offering_range_lower_limit"`
 	// 희망공모가 상한
 	OfferingRangeHigherLimit int64 `json:"offering_range_higher_limit"`
 	// 경쟁률
-	CompetitiveRate2f int64 `json:"competitive_rate_2f"`
+	CompetitiveRate2f *int64 `json:"competitive_rate_2f"`
 	// 주간사
 	LeadManagers []string `json:"lead_manager"`
 }
 
-var data = []IpoMessage{
-	{
-		ItemName:                 "에스디바이오센서",
-		TickerCode:               "137310",
-		StartDateMs:              1625702400000, // 2021.07.08
-		EndDateMs:                1625788800000, // 2021.07.09
-		RefundDateMs:             1626134400000, // 2021.07.13
-		ListingDateMs:            1626393600000, // 2021.07.16
-		FinalOfferingPrice:       52000,
-		OfferingRangeLowerLimit:  45000,
-		OfferingRangeHigherLimit: 52000,
-		CompetitiveRate2f:        27402,
-		LeadManagers: []string{
-			"NH투자증권", "한국투자증권", "삼성증권", "KB증권",
-		},
-	},
+const fileName = "ipos.json"
+
+func GetIpoData() ([]*IpoMessage, error) {
+	jsonFile, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var ipos []*IpoMessage
+	if err := json.Unmarshal(byteValue, &ipos); err != nil {
+		return nil, err
+	}
+
+	return ipos, nil
 }
--- function.go
+++ function.go
@@ -9,6 +9,11 @@ import (
 func ListIpos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
+	data, err := GetIpoData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatal(err)
 	}
